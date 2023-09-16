48fd918c014d11dbc05f6edb9435429b43ace88c
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-09-16 14:05:32 +0200

    initial commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,62 +1,43 @@
 #!/usr/bin/env bash
-# Installs, configures, and starts the web server
-SERVER_CONFIG="server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
+# a Bash script that sets up your web servers for the deployment of web_static.
 
-	server_name _;
-	index index.html index.htm;
-	error_page 404 /404.html;
-	add_header X-Served-By \$hostname;
-
-	location / {
-		root /var/www/html/;
-		try_files \$uri \$uri/ =404;
-	}
-
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-		try_files \$uri \$uri/ =404;
-	}
-
-	if (\$request_filename ~ redirect_me) {
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
-	}
-
-	location = /404.html {
-		root /var/www/error/;
-		internal;
-	}
-}"
-HOME_PAGE="<!DOCTYPE html>
-<html lang='en-US'>
-	<head>
-		<title>Home - AirBnB Clone</title>
-	</head>
-	<body>
-		<h1>Welcome to AirBnB!</h1>
-	<body>
-</html>
-"
-# shellcheck disable=SC2230
-if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
-    apt-get update
-    apt-get -y install nginx
+# Installing Nginx and configuring firewall
+if ! command -v nginx; then
+    sudo apt-get update &>/dev/null
+    echo "Installing Nginx"
+    sudo apt-get -y install nginx &>/dev/null
+    sudo uwf allow 'Nginx HTTP' &>/dev/null
 fi
-mkdir -p /var/www/html /var/www/error
-chmod -R 755 /var/www
-echo 'Hello World!' > /var/www/html/index.html
-echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
 
-mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo -e "$HOME_PAGE" > /data/web_static/releases/test/index.html
-[ -d /data/web_static/current ] && rm -rf /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data
-bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
-ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-if [ "$(pgrep -c nginx)" -le 0 ]; then
-	service nginx start
+# configure the Nginx config file to deploy static files
+dir_1="/data/web_static/releases/test/"
+symlink="/data/web_static/current"
+dir_2="/data/web_static/shared/"
+
+if ! [ -d "$dir_1" ]; then
+    sudo mkdir -p "$dir_1"
+    sudo sh -c 'echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html'
+fi
+
+if ! [ -d "$dir_2" ]; then
+        sudo mkdir -p "$dir_2"
+fi
+
+if [ -e "$symlink" ]; then
+    sudo rm -rf "$symlink"
+    sudo ln -s "$dir_1" "$symlink"
 else
-	service nginx restart
+    sudo ln -s "$dir_1" "$symlink"
 fi
+
+sudo chown -R ubuntu:ubuntu /data/
+
+if ! cat < /etc/nginx/sites-enabled/default | grep -q "olisabelema.tech"; then
+    sudo sed -i '0,/^\(\s*\)server_name\s*.*$/s//\1server_name olisabelema.tech www.olisabelema.tech;/' /etc/nginx/sites-available/default
+    sudo sed -i '0,/^\(\s*\)server_name olisabelema.tech www.olisabelema.tech;$/s//&\n\n\1location \/hbnb_static {\n\1\1alias \/data\/web_static\/current\/;\n\1\1autoindex off;\n\1}/' /etc/nginx/sites-available/default
+fi
+
+# Restarting Nginx
+sudo service nginx restart;
+
+echo "All Done!"
